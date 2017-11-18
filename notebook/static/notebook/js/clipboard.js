083f2c8b66d0a893642aa04b67a9e0aa05809169
madhu94 <galt4000@gmail.com>
2017-11-18 21:37:57 +0530

    Allow elements to side-step notebook's copy cells feature

--- notebook/static/notebook/js/clipboard.js
+++ notebook/static/notebook/js/clipboard.js
@@ -37,7 +37,8 @@ function load_json(clipboard) {
 function copy(event) {
   if ((Jupyter.notebook.mode !== 'command') ||
         // window.getSelection checks if text is selected, e.g. in output
-        !window.getSelection().isCollapsed) {
+        !window.getSelection().isCollapsed ||
+        event.target.classList.contains('notebook-copy-ignore')) {
     return;
   }
   var selecn = Jupyter.notebook.get_selected_cells().map(
